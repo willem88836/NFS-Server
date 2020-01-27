682c64afb6c624c801ce6b6618b06b12470ccb9a
Willem Meijer <willem1998m@hotmail.com>
2020-01-27 15:05:29 +0100

    Directory content is loaded

--- src/ClientView.py
+++ src/ClientView.py
@@ -1,15 +1,14 @@
+from HandleTypes import HandleTypes
 from NfsClient import *
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import Scrollbar
-from HandleTypes import HandleTypes
 
 
 class ClientView(NfsClient):
     # contains truple: tab, server, and current directory
     tabs = []
-    dirClickVar = None
 
     def __init__(self):
         NfsClient.__init__(self)
@@ -19,28 +18,47 @@ class ClientView(NfsClient):
         print("Client View Initialized")
         self.view.mainloop()
 
+    def OnDirectoryClick(self, mouseEvent):
+        listbox = mouseEvent.widget
+        selectionIndex = mouseEvent.widget.curselection()
+        selection = listbox.get(selectionIndex)
+        
+        truple = None
+        for t in self.tabs:
+            for o in t[0].winfo_children():
+                if o == listbox:
+                    truple = t
+                    break
+            if not truple == None:
+                break
+
+        truple[2] += selection
+        self.RequestFillView(truple)
+
     def BuildTabs(self):
         tabControl = ttk.Notebook(self.view)
         for connection in self.connections:
+            self.BuildTab(connection, tabControl)
+        tabControl.pack(expand=1, fill="both")
+
+    def BuildTab(self, connection, tabControl):
             tab = ttk.Frame(tabControl)
             tabControl.add(tab, text=connection.GetAddress())
-            tabControl.pack(expand=1, fill="both")
             
+            scrollbar = ttk.Scrollbar(tab)
+            scrollbar.pack(side=RIGHT, fill=Y)
+
+            listbox = Listbox(tab, yscrollcommand=scrollbar.set, selectmode=SINGLE)
+            listbox.bind("<Double-Button-1>", self.OnDirectoryClick)
+            listbox.pack(side=LEFT, fill=BOTH)
+
             truple = [tab, connection, ""]
             self.tabs.append(truple)
-
-            scrollbar = Scrollbar(tab)
-            scrollbar.pack(side = RIGHT, fill = Y)
-            self.myList = Listbox(tab, yscrollcommand=scrollbar.set)
             self.RequestFillView(truple)
 
     def RequestFillView(self, truple):
         message = RpcMessage(HandleTypes.RequestDirectoryContents, truple[2])
         truple[1].SendMessage(message)
-
-
-    def OnDirectoryClicked(self):
-        print(self.dirClickVar)
 
     def OnDirectoryContentsReceived(self, server, args):
         truple = None
@@ -56,8 +74,16 @@ class ClientView(NfsClient):
         #TODO: Magic!
         args = args[7:len(args)-3].split("', '")
         
-        col = 0
-        for d in args:
-            dirButton = ttk.Button(truple[0], text=d, variable=self.dirClickVar, command=self.OnDirectoryClicked)
-            # dirButton.grid(column=col, row = 0)
-            col += 1
+        tab = truple[0]
+        
+        listbox = tab.winfo_children()
+        for o in listbox:
+            if o.widgetName == "listbox":
+                listbox = o
+                break
+        
+        listbox.delete(0, END)
+
+        for i in range(len(args)):
+            listbox.insert(i, args[i])
+            
--- src/TkInterSubSystem.py
+++ src/TkInterSubSystem.py
@@ -2,83 +2,49 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import Scrollbar
+from tkinter import *
+
+
+def OnClick(a):
+    print(a)
+
+
 
 
 root = tk.Tk() 
-
 root.title("Wizard")
-root.resizable(False, False)
 
-#label
-a_label = ttk.Label(root, text="Enter a name:")
-a_label.grid(column=0, row=0)
-print(a_label)
+tabControl = ttk.Notebook(root)
+for a in range(5):
+    tab = ttk.Frame(tabControl)
+    tabControl.add(tab, text="tab_%i"%a)
 
-def click_me():
-    action.configure(text="I have been clicked")
-    a_label.configure(foreground="red")
-    a_label.configure(text="Hi, %s %s!" % (name.get(), number.get()))
+    scrollbar = ttk.Scrollbar(tab)
+    scrollbar.pack(side=RIGHT, fill=Y)
 
-#inputfield
-name = tk.StringVar()
-name_entered = ttk.Entry(root, width=12, textvariable=name)
-name_entered.grid(column=0, row=1)
+    lb = Listbox(tab, yscrollcommand=scrollbar.set, selectmode=SINGLE)
+    lb.bind('<Double-Button-1>', OnClick)
+    lb.pack(side=LEFT, fill=BOTH)
 
-#button
-action = ttk.Button(root, text="click me", command=click_me)
-action.grid(column=1, row=1)
-action.configure(state='enabled')
+    for b in range(20):
+        # label = ttk.Label(lb, text="this is a label")
+        lb.insert(b, "label_%i"%b)
+        # label.grid(column = 0, row = b*2, sticky='W')
 
+        # button = ttk.Button(lb, text="button_%i"%b)
+        # lb.insert(button)
+        # button.grid(column = 0, row = b*2+1, sticky="W")
 
-ttk.Label(root, text="Choose a number:").grid(column=0, row=2)
-#dropdown
-number = tk.StringVar()
-number_chosen = ttk.Combobox(root, width = 12, textvariable=number, state="readonly")
-number_chosen['values'] = (1, 2, 4, 8, 16, 32)
-number_chosen.grid(column=1, row = 2)
-number_chosen.current(3)
-
-#checkbutton
-chVar = tk.IntVar()
-check1 = tk.Checkbutton(root, text="checkbutton!", variable=chVar, state="active")
-check1.grid(column=0, row=4, sticky=tk.W)
-check1.select()
+    # labelfr = ttk.Labelframe(tab, text = "main frame")
+    # labelfr.grid(column = 0, row = 0, padx = 20, pady = 20)
+    # for b in range(20):
+    #     label = ttk.Label(labelfr, text="enter a name")
+    #     label.grid(column = 0, row = b*2, sticky='W')
+    #     button = ttk.Button(labelfr, text="button_%i"%b)
+    #     button.grid(column = 0, row = b*2+1, sticky="W")
 
 
-#radiobutton
-def RadCall():
-    colors = ["Green", "Blue", "Gold"]
-    a_label.configure(foreground=colors[rbVar.get()])
-
-
-rbVar = tk.IntVar()
-
-rad1=tk.Radiobutton(root, text="Color1", variable = rbVar, value = 0, command=RadCall)
-rad1.grid(column=0, row = 5, sticky=tk.W, columnspan=3)
-
-rad2=tk.Radiobutton(root, text="Color2", variable = rbVar, value = 1, command=RadCall)
-rad2.grid(column=1, row = 5, sticky=tk.W, columnspan=3)
-
-rad3=tk.Radiobutton(root, text="Color3", variable = rbVar, value = 2, command=RadCall)
-rad3.grid(column=2, row = 5, sticky=tk.W, columnspan=3)
-
-
-scroll_w = 30
-scroll_h = 3
-scr=scrolledtext.ScrolledText(root, width = scroll_w, height=scroll_h, wrap=tk.WORD)
-scr.grid(column=0, row=6)
-
-
-buttons_frame = ttk.Labelframe(root, text="labels in a frame")
-buttons_frame.grid(column=0, row=7)
-
-
-for i in range(5):
-    ttk.Label(buttons_frame, text="Label_%s" % str(i)).grid(column=i, row=0, sticky=tk.W)
-
-
-
-# ttk.Label(buttons_frame,text="Label1")
+tabControl.pack(expand=1, fill="both")
 
 
 
@@ -86,6 +52,90 @@ for i in range(5):
 
 
 
+# root.resizable(False, False)
 
-name_entered.focus() #sets that field to be automatically selected 
+# #label
+# a_label = ttk.Label(root, text="Enter a name:")
+# a_label.grid(column=0, row=0)
+# print(a_label)
+
+# def click_me():
+#     action.configure(text="I have been clicked")
+#     a_label.configure(foreground="red")
+#     a_label.configure(text="Hi, %s %s!" % (name.get(), number.get()))
+
+# #inputfield
+# name = tk.StringVar()
+# name_entered = ttk.Entry(root, width=12, textvariable=name)
+# name_entered.grid(column=0, row=1)
+
+# #button
+# action = ttk.Button(root, text="click me", command=click_me)
+# action.grid(column=1, row=1)
+# action.configure(state='enabled')
+
+
+# ttk.Label(root, text="Choose a number:").grid(column=0, row=2)
+# #dropdown
+# number = tk.StringVar()
+# number_chosen = ttk.Combobox(root, width = 12, textvariable=number, state="readonly")
+# number_chosen['values'] = (1, 2, 4, 8, 16, 32)
+# number_chosen.grid(column=1, row = 2)
+# number_chosen.current(3)
+
+# #checkbutton
+# chVar = tk.IntVar()
+# check1 = tk.Checkbutton(root, text="checkbutton!", variable=chVar, state="active")
+# check1.grid(column=0, row=4, sticky=tk.W)
+# check1.select()
+
+
+# #radiobutton
+# def RadCall():
+#     colors = ["Green", "Blue", "Gold"]
+#     a_label.configure(foreground=colors[rbVar.get()])
+
+
+# rbVar = tk.IntVar()
+
+# rad1=tk.Radiobutton(root, text="Color1", variable = rbVar, value = 0, command=RadCall)
+# rad1.grid(column=0, row = 5, sticky=tk.W, columnspan=3)
+
+# rad2=tk.Radiobutton(root, text="Color2", variable = rbVar, value = 1, command=RadCall)
+# rad2.grid(column=1, row = 5, sticky=tk.W, columnspan=3)
+
+# rad3=tk.Radiobutton(root, text="Color3", variable = rbVar, value = 2, command=RadCall)
+# rad3.grid(column=2, row = 5, sticky=tk.W, columnspan=3)
+
+
+# scroll_w = 30
+# scroll_h = 3
+# scr=scrolledtext.ScrolledText(root, width = scroll_w, height=scroll_h, wrap=tk.WORD)
+# scr.grid(column=0, row=6)
+
+
+# buttons_frame = ttk.Labelframe(root, text="labels in a frame")
+# buttons_frame.grid(column=0, row=7)
+
+
+# for i in range(5):
+#     ttk.Label(buttons_frame, text="Label_%s" % str(i)).grid(column=i, row=0, sticky=tk.W)
+
+
+
+# # ttk.Label(buttons_frame,text="Label1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name_entered.focus() #sets that field to be automatically selected 
 root.mainloop()
