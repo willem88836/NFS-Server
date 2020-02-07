3b56d4883bfc73b8924dea487a525318408c1822
Willem Meijer <willem1998m@hotmail.com>
2020-02-07 16:31:20 +0100

    Insert pseudo code FTP

--- src/RpcServerThread.py
+++ src/RpcServerThread.py
@@ -49,3 +49,54 @@ class RpcServerThread(threading.Thread):
     def Terminate(self):
         self.isConnected = False
         
+
+
+
+
+
+
+
+
+
+    ## Pseudo-code. 
+
+    STREAM_BUFFER = 65535 # cannot be more than 65535 (that's the general PC buffersize).. 
+    cur_buffer_use = 0 # This number will probably exceed the max..
+    MAXIMUM_SEGMENT_SIZE = 1480 # Double check this size. calculate the headers.
+
+
+    def TransmitFile(self, path):
+        #mostly pseudo-code.
+
+        file = open(path, mode="r")
+        fileSize = os.path.getsize(path) 
+
+        fileChunkIndex = 0
+        fileChunksTotal = fileSize / self.MAXIMUM_SEGMENT_SIZE # The total number of chunks inside the file.
+        lastChunkSize = fileSize % self.MAXIMUM_SEGMENT_SIZE
+
+        fileSize = 0 # fill this in. s
+        streamedFileSize = 0 
+
+        
+        while streamedFileSize < fileSize:
+            # Should be streaming.
+
+            while self.cur_buffer_use >= self.STREAM_BUFFER:
+                ## wait for a while, or till the buffer is empty enough.
+                continue
+            
+            file.seek(fileChunkIndex * self.MAXIMUM_SEGMENT_SIZE)
+            readLength = -1
+            if fileChunkIndex == fileChunksTotal:
+                readLength = lastChunkSize
+            else:
+                readLength = self.MAXIMUM_SEGMENT_SIZE
+
+            fileChunkIndex += 1
+            self.cur_buffer_use += readLength
+            
+            dataChunk = file.read(readLength)
+            # send dataChunk.
+
+        file.close()
